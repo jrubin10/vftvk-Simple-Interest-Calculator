e0d51bcc61468b50aefd43701bd7b246c8880100
Jroobz <jrubin10@gmail.com>
2021-05-20 00:20:28 +0000

    Exercise 4 Step 13 Pending

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+//Need to finish step 13 in exercise 4 to highlight the numbers
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year=new Date().getFullYear()+parseInt(years);
+    var interestExplanation="If you deposit " +principal+",\nat an interest rate of "+rate+",\nYou will receive an amount of "+interest+",\nin the year "+year;
+    if (principal<=0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+    document.getElementById("result").innerText=interestExplanation;
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
