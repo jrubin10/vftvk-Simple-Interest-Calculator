5bb0fc4dab51645fc5a177918fa112c15ecf1210
Jroobz <jrubin10@gmail.com>
2021-05-22 17:19:47 +0000

    Done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year=new Date().getFullYear()+parseInt(years);
-    var interestExplanation="If you deposit <span style='color:red'>" +principal+"</span>,\<br\>at an interest rate of <span style='color:red'>"+rate+"%</span>,\<br\>You will receive an amount of <span style='color:red'>"+interest+"</span>,\<br\>in the year <span style='color:red'>"+year+"</span>";
+    var interestExplanation="If you deposit <span style='background-color: #FFFF00'>" +principal+"</span>,\<br\>at an interest rate of <span style='background-color: #FFFF00'>"+rate+"%</span>,\<br\>You will receive an amount of <span style='background-color: #FFFF00'>"+interest+"</span>,\<br\>in the year <span style='background-color: #FFFF00'>"+year+"</span>";
     if (principal<=0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
